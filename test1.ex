94117eefca1f77e68c4e79648a3275f6f56e91c1
Thil Bandara <thil.bandara@redbubble.com>
2016-05-27 11:24:58 +1000

    add Exercises

---
+++ test1.ex
@@ -0,0 +1,7 @@
+# Create a method that will sum a list
+# functions are defined by
+#
+# def sum() do
+# end
+defmodule Test do
+end
---
+++ test2.ex
@@ -0,0 +1,7 @@
+# For the numbers 1 - 100, print Fizz if it is divisible by 3, Buzz if it is divisible by 5, FizzBuzz if divisible by 3 and 5
+# and just print the number if none of those are true
+#
+# Can use `h rem` to find divisible
+
+defmodule FizzBuzz do
+end
